bc0f6d06d9d31c07989be7e5435a28702cfb7ad1
aliceout <github.underpay178@slmail.me>
2022-08-28 13:55:44 +0200

    Update index.php

---
+++ assets/js/config.js
@@ -0,0 +1,6 @@
+"use strict";
+// config vars
+var newsletterType = "mailchimp"; // "mailchimp" for mailchimp, "email" to recieve email subscriber to your inbox
+var newsletterEmail = "example@example.tld"; // in case you choose newsletter by "email"
+var mailchimpAjaxLink = "http://publicthemes.us7.list-manage.com/subscribe/post-json?u=0e55f682ed9d61847d065bf85&amp;id=793c157ca4&c=?"; // only for mailchimp
+var newsletterSuccessMsg = "Awesome! We sent you a confirmation email."; //success message after user successfull subscribe.
