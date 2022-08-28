6d6da84df999b29ad957fce0e2b5bd9deaa85d5e
aliceout <github.underpay178@slmail.me>
2022-08-28 13:55:44 +0200

    Update index.php

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ include('assets/php/partials/head.php');
                             </div>
                             <div class="row">
                                 <div class="col-md-12">
-                                    <p>Gestion de cycles de projets, mojets, renforcement et partenariats<br><br>
+                                    <p>Cycle de projets, outils de suivi et de gestion de projets, <br><br>
                                         <a href="education.php">En savoir plus</a>
                                     </p>
                                 </div>
