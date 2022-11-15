ae4cc670bc37c22479bf8fccc4dc03fa8fa3c56c
aliceout <github.underpay178@slmail.me>
2022-11-15 21:05:01 +0100

    Beta 2
    Suppression du top et déplacement dans la sidebar
    Correction du javascript
    
    Correctioon du sticky sidebar

--- fr/benevolat.php
+++ fr/benevolat.php
@@ -8,115 +8,104 @@
 
     <body>
         <div class="container">
-            <div class="section">
+            <div id="section" class="section">
 
                 <div id="big-main" class="columns is-multiline">
                     <?php include('sidebar.php'); ?>
                     <main class="column">
 
-                        <div class="columns is-multiline">
-                            <?php include('top.php'); ?>
-                            <div class="column is-12">
+                        <section class="card sub-main">
+                            <div class="card-content">
 
-                                <div class="columns"> 
-                                    <section class="column">
+                                <h2>Experiences benevole</h2>
+                                <div class="experience">
 
-                                        <div class="card top-card">
-                                            <div class="card-content">
-
-                                                <h2>Experiences benevole</h2>
-                                                <div class="experience">
-
-                                                    <div class="job">
-                                                        <div class="card-content">
-                                                            <div class="media">
-                                                                <div class="media-content">
-                                                                    <div class="jobTitle"><h5>Médecins du Monde</h5>
-                                                                        <div class="lieu">Marseille, <strong>France</strong>&emsp;<i class="fas fa-map-marker-alt"></i></div>
-                                                                    </div>
-                                                                    <h6 class="position">Bénévole support administratif · Logistique, Finance &amp; RH</h6>
-                                                                    <div class="date">2019 - 2020</div>
-                                                                </div>
-                                                                <div class="media-left">
-                                                                    <div class="logo"><img src="../assets/images/mdm2.png"></div>
-                                                                </div>
-                                                            </div>
-                                                            <div class="content"><p>Programme “Migrations frontières transalpines”<br><br>
-                                                                Tâches de support en Ressources humaines (rédaction de profils de mission, organigramme, tenue base de données), Logistique (gestion de stock, gestion parc véhicules) Finances (suivi budgétaire, suivi des frais bénevoles) et informatique (Mise en oeuvre d’outils collaboratifs)</p>
-                                                            </div>
-                                                        </div>
+                                    <div class="job">
+                                        <div class="card-content">
+                                            <div class="media">
+                                                <div class="media-content">
+                                                    <div class="jobTitle"><h5>Médecins du Monde</h5>
+                                                        <div class="lieu">Marseille, <strong>France</strong>&emsp;<i class="fas fa-map-marker-alt"></i></div>
                                                     </div>
-
-                                                    <div class="divider is-info is-light"></div>
-
-                                                    <div class="job">
-                                                        <div class="card-content">
-                                                            <div class="media">
-                                                                <div class="media-content">
-                                                                    <div class="jobTitle"><h5>LA CIMADE</h5>
-                                                                        <div class="lieu">Lyon, <strong>France</strong>&emsp;<i class="fas fa-map-marker-alt"></i></div>
-                                                                    </div>
-                                                                    <h6 class="position">Permanente en droit des étrangers, co-animateur Pôle "Plaidoyer, sensibilisation et solidarités internationales"</h6>
-                                                                    <div class="date">2017 - 2019</div>
-                                                                </div>
-                                                                <div class="media-left">
-                                                                    <div class="logo"><img src="../assets/images/cimade.png"></div>
-                                                                </div>
-                                                            </div>
-                                                            <div class="content"><p>Permanences d’accès aux droits · Plaidoyer auprès de parlementaires dans cadre loi Asile et immigration · Élaboration de supports de communication dans le cadre du festival "Migrant'Scène" · Actions de sensibilisation, et plaidoyer · Refonte et réorganisation des rôles et missions du pôle “SI, sensibilisation, et plaidoyer” · Activité de supports interne sur les outils numériques</p>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-
-                                                    <div class="divider is-info is-light"></div>
-
-                                                    <div class="job">
-                                                        <div class="card-content">
-                                                            <div class="media">
-                                                                <div class="media-content">
-                                                                    <div class="jobTitle"><h5>LA CIMADE</h5>
-                                                                        <div class="lieu">Toulouse, <strong>France</strong>&emsp;<i class="fas fa-map-marker-alt"></i></div>
-                                                                    </div>
-                                                                    <h6 class="position">Permanente en droit des étrangers</h6>
-                                                                    <div class="date">2015 - 2017</div>
-                                                                </div>
-                                                                <div class="media-left">
-                                                                    <div class="logo"><img src="../assets/images/cimade.png"></div>
-                                                                </div>
-                                                            </div>
-                                                            <div class="content"><p>Permanences d’accès aux droits. Élaboration et mise en page des rapports d’activités régionaux et du festival Migrant’scène, missions internes de support</p>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-
-                                                    <div class="divider is-info is-light"></div>
-
-                                                    <div class="job">
-                                                        <div class="card-content">
-                                                            <div class="media">
-                                                                <div class="media-content">
-                                                                    <div class="jobTitle"><h5>ASSOCIATION A BUT POLITIQUE</h5>
-                                                                        <div class="lieu">Orléans, <strong>France</strong>&emsp;<i class="fas fa-map-marker-alt"></i></div>
-                                                                    </div>
-                                                                    <h6 class="position">Militante et responsable communication</h6>
-                                                                    <div class="date">2010-2012 · 2 ans</div>
-                                                                </div>
-                                                            </div>
-                                                            <div class="content"><p>Création site web, écriture articles de blog, création de supports de communication<br>
-                                                                Animation de réunions, tâches administratives, tractage, co-organisation de réunions publiques</p>
-                                                            </div>
-                                                            <div class="media-left">
-                                                            </div>
-                                                        </div>
-                                                    </div>
-
+                                                    <h6 class="position">Bénévole support administratif · Logistique, Finance &amp; RH</h6>
+                                                    <div class="date">2019 - 2020</div>
+                                                </div>
+                                                <div class="media-left">
+                                                    <div class="logo"><img src="../assets/images/mdm2.png"></div>
                                                 </div>
                                             </div>
+                                            <div class="content"><p>Programme “Migrations frontières transalpines”<br><br>
+                                                Tâches de support en Ressources humaines (rédaction de profils de mission, organigramme, tenue base de données), Logistique (gestion de stock, gestion parc véhicules) Finances (suivi budgétaire, suivi des frais bénevoles) et informatique (Mise en oeuvre d’outils collaboratifs)</p>
+                                            </div>
                                         </div>
-                                    </section>
+                                    </div>
+
+                                    <div class="divider is-info is-light"></div>
+
+                                    <div class="job">
+                                        <div class="card-content">
+                                            <div class="media">
+                                                <div class="media-content">
+                                                    <div class="jobTitle"><h5>LA CIMADE</h5>
+                                                        <div class="lieu">Lyon, <strong>France</strong>&emsp;<i class="fas fa-map-marker-alt"></i></div>
+                                                    </div>
+                                                    <h6 class="position">Permanente en droit des étrangers, co-animateur Pôle "Plaidoyer, sensibilisation et solidarités internationales"</h6>
+                                                    <div class="date">2017 - 2019</div>
+                                                </div>
+                                                <div class="media-left">
+                                                    <div class="logo"><img src="../assets/images/cimade.png"></div>
+                                                </div>
+                                            </div>
+                                            <div class="content"><p>Permanences d’accès aux droits · Plaidoyer auprès de parlementaires dans cadre loi Asile et immigration · Élaboration de supports de communication dans le cadre du festival "Migrant'Scène" · Actions de sensibilisation, et plaidoyer · Refonte et réorganisation des rôles et missions du pôle “SI, sensibilisation, et plaidoyer” · Activité de supports interne sur les outils numériques</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="divider is-info is-light"></div>
+
+                                    <div class="job">
+                                        <div class="card-content">
+                                            <div class="media">
+                                                <div class="media-content">
+                                                    <div class="jobTitle"><h5>LA CIMADE</h5>
+                                                        <div class="lieu">Toulouse, <strong>France</strong>&emsp;<i class="fas fa-map-marker-alt"></i></div>
+                                                    </div>
+                                                    <h6 class="position">Permanente en droit des étrangers</h6>
+                                                    <div class="date">2015 - 2017</div>
+                                                </div>
+                                                <div class="media-left">
+                                                    <div class="logo"><img src="../assets/images/cimade.png"></div>
+                                                </div>
+                                            </div>
+                                            <div class="content"><p>Permanences d’accès aux droits. Élaboration et mise en page des rapports d’activités régionaux et du festival Migrant’scène, missions internes de support</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="divider is-info is-light"></div>
+
+                                    <div class="job">
+                                        <div class="card-content">
+                                            <div class="media">
+                                                <div class="media-content">
+                                                    <div class="jobTitle"><h5>ASSOCIATION A BUT POLITIQUE</h5>
+                                                        <div class="lieu">Orléans, <strong>France</strong>&emsp;<i class="fas fa-map-marker-alt"></i></div>
+                                                    </div>
+                                                    <h6 class="position">Militante et responsable communication</h6>
+                                                    <div class="date">2010-2012 · 2 ans</div>
+                                                </div>
+                                            </div>
+                                            <div class="content"><p>Création site web, écriture articles de blog, création de supports de communication<br>
+                                                Animation de réunions, tâches administratives, tractage, co-organisation de réunions publiques</p>
+                                            </div>
+                                            <div class="media-left">
+                                            </div>
+                                        </div>
+                                    </div>
+
                                 </div>
                             </div>
-                        </div>
+                        </section>
                     </main>
                 </div>
             </div>
