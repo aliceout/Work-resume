2f0444e69573d532aa23eb1d3b138e03c19249e6
aliceout <github.underpay178@slmail.me>
2023-05-13 12:26:21 +0200

    Correction de div

--- fr/education.php
+++ fr/education.php
@@ -48,112 +48,111 @@ include('../assets/php/header.php');
                                     </ul>
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="divider is-info is-light"></div>
+                            <div class="divider is-info is-light"></div>
 
-                        <div class="graduation">
-                            <div class="card-content">
-                                <div class="media">
-                                    <div class="media-content">
-                                        <div class="graduationTitle">
-                                            <h5>Master II · Politiques publiques · Action Humanitaire Internationale</h5>
+                            <div class="graduation">
+                                <div class="card-content">
+                                    <div class="media">
+                                        <div class="media-content">
+                                            <div class="graduationTitle">
+                                                <h5>Master II · Politiques publiques · Action Humanitaire Internationale</h5>
+                                            </div>
+                                            <div class="lieu">Université Paris-Est Créteil</div>
+                                            <span class="date">2018 - 2019</span>
                                         </div>
-                                        <div class="lieu">Université Paris-Est Créteil</div>
-                                        <span class="date">2018 - 2019</span>
+                                        <div class="media-left">
+                                            <div class="logo"><img src="../assets/images/upec.png"></div>
+                                        </div>
                                     </div>
-                                    <div class="media-left">
-                                        <div class="logo"><img src="../assets/images/upec.png"></div>
+                                    <div class="content">
+                                        <p><strong>Option</strong> : Urgence et réhabilitation<br>
+                                            Diplomée mention AB</p>
+                                        <p>Gestion de cycles de projets, monitoring qualité, évaluation et capitalisation <br>
+                                            Cas pays, mises en situation, conceptions de projets, renforcement et partenariats<br><br>
+                                            <strong>Mémoire de stage I</strong>: <i>"Les TIC dans le secteur de la solidarité internationale, réflexions sur une intégration à marche forcée"</i><br>
+                                            <strong>Mémoire de stage II</strong>: <i>"La notion de crise et son utilisation dans la thématique migratoires, d'un outil de mobilisation à l'abus d'un objet devenue politique"</i>
+                                        </p>
                                     </div>
                                 </div>
-                                <div class="content">
-                                    <p><strong>Option</strong> : Urgence et réhabilitation<br>
-                                        Diplomée mention AB</p>
-                                    <p>Gestion de cycles de projets, monitoring qualité, évaluation et capitalisation <br>
-                                        Cas pays, mises en situation, conceptions de projets, renforcement et partenariats<br><br>
-                                        <strong>Mémoire de stage I</strong>: <i>"Les TIC dans le secteur de la solidarité internationale, réflexions sur une intégration à marche forcée"</i><br>
-                                        <strong>Mémoire de stage II</strong>: <i>"La notion de crise et son utilisation dans la thématique migratoires, d'un outil de mobilisation à l'abus d'un objet devenue politique"</i>
-                                    </p>
+                            </div>
+
+                            <div class="divider is-info is-light"></div>
+
+                            <div class="graduation">
+                                <div class="card-content">
+                                    <div class="media">
+                                        <div class="media-content">
+                                            <div class="graduationTitle">
+                                                <h5>Master I · Sciences Politiques</h5>
+                                            </div>
+                                            <div class="lieu">Université Lyon III Jean Moulin</div>
+                                            <span class="date">2017 - 2018</span>
+                                        </div>
+                                        <div class="media-left">
+                                            <div class="logo"><img src="../assets/images/lyon3.png"></div>
+                                        </div>
+                                    </div>
+                                    <div class="content">
+                                        <p><strong>Mention</strong> : Relations Internationales <br>
+                                            Diplômée<br><br>
+                                            Simulation de gestion de crise internationale, Aires culturelles mondiales<br>
+                                            Religions et mondialisation, Economie politique européenne, Politiques étrangères comparées<br><br>
+                                            <strong>Mémoire</strong> : <i>"L’externalisation des contrôles migratoires au Sahel. Étude sur un nouvel outil d’une politique européenne extra territorialisée de gestion des frontières"</i>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="divider is-info is-light"></div>
+
+                            <div class="graduation">
+                                <div class="card-content">
+                                    <div class="media">
+                                        <div class="media-content">
+                                            <div class="graduationTitle">
+                                                <h5>Licence · Droit</h5>
+                                            </div>
+                                            <div class="lieu">Université Toulouse I Capitole</div>
+                                            <span class="date">2015 - 2017</span>
+                                        </div>
+                                        <div class="media-left">
+                                            <div class="logo"><img src="../assets/images/toulouse1.png"></div>
+                                        </div>
+                                    </div>
+                                    <div class="content">
+                                        <p><strong>Mention</strong> : Droit Public<br>Diplomée mention AB<br><br>
+                                            Dominante en droit administratif, droit international et droit européen</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="divider is-info is-light"></div>
+
+                            <div class="graduation">
+                                <div class="card-content">
+                                    <div class="media">
+                                        <div class="media-content">
+                                            <div class="graduationTitle">
+                                                <h5>Capacité en Droit</h5>
+                                            </div>
+                                            <div class="lieu">Conservatoire national des arts et métiers</div>
+                                            <span class="date">2013 - 2015</span>
+                                        </div>
+                                        <div class="media-left">
+                                            <div class="logo"><img src="../assets/images/cnam.png"></div>
+                                        </div>
+                                    </div>
+                                    <div class="content">
+                                        <p>Diplômee délivré par l'Université Toulouse I Capitole <br>Diplomé mention B</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-
-                        <div class="divider is-info is-light"></div>
-
-                        <div class="graduation">
-                            <div class="card-content">
-                                <div class="media">
-                                    <div class="media-content">
-                                        <div class="graduationTitle">
-                                            <h5>Master I · Sciences Politiques</h5>
-                                        </div>
-                                        <div class="lieu">Université Lyon III Jean Moulin</div>
-                                        <span class="date">2017 - 2018</span>
-                                    </div>
-                                    <div class="media-left">
-                                        <div class="logo"><img src="../assets/images/lyon3.png"></div>
-                                    </div>
-                                </div>
-                                <div class="content">
-                                    <p><strong>Mention</strong> : Relations Internationales <br>
-                                        Diplômée<br><br>
-                                        Simulation de gestion de crise internationale, Aires culturelles mondiales<br>
-                                        Religions et mondialisation, Economie politique européenne, Politiques étrangères comparées<br><br>
-                                        <strong>Mémoire</strong> : <i>"L’externalisation des contrôles migratoires au Sahel. Étude sur un nouvel outil d’une politique européenne extra territorialisée de gestion des frontières"</i>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="divider is-info is-light"></div>
-
-                        <div class="graduation">
-                            <div class="card-content">
-                                <div class="media">
-                                    <div class="media-content">
-                                        <div class="graduationTitle">
-                                            <h5>Licence · Droit</h5>
-                                        </div>
-                                        <div class="lieu">Université Toulouse I Capitole</div>
-                                        <span class="date">2015 - 2017</span>
-                                    </div>
-                                    <div class="media-left">
-                                        <div class="logo"><img src="../assets/images/toulouse1.png"></div>
-                                    </div>
-                                </div>
-                                <div class="content">
-                                    <p><strong>Mention</strong> : Droit Public<br>Diplomée mention AB<br><br>
-                                        Dominante en droit administratif, droit international et droit européen</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="divider is-info is-light"></div>
-
-                        <div class="graduation">
-                            <div class="card-content">
-                                <div class="media">
-                                    <div class="media-content">
-                                        <div class="graduationTitle">
-                                            <h5>Capacité en Droit</h5>
-                                        </div>
-                                        <div class="lieu">Conservatoire national des arts et métiers</div>
-                                        <span class="date">2013 - 2015</span>
-                                    </div>
-                                    <div class="media-left">
-                                        <div class="logo"><img src="../assets/images/cnam.png"></div>
-                                    </div>
-                                </div>
-                                <div class="content">
-                                    <p>Diplômee délivré par l'Université Toulouse I Capitole <br>Diplomé mention B</p>
-                                </div>
-                            </div>
-                        </div>
+                    </section>
+                </main>
             </div>
-            </section>
-            </main>
         </div>
-    </div>
     </div>
 </body>
 <?php include('../assets/php/footer.php'); ?>
