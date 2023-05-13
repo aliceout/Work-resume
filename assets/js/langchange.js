6bc6e34202e5e52490ac8e783a17b8576cf914c7
aliceout <github.underpay178@slmail.me>
2023-05-13 16:07:05 +0200

    Optimisation du changeur de langue

--- assets/js/langchange.js
+++ assets/js/langchange.js
@@ -1,66 +1,55 @@
-const langChangeToggle = document.getElementById('language-checkbox');                                                  // Obtenir l'état de la chebox
-const currentURL = document.location.href.replace(/\/$/, "");                                                           // Obtenir l'URL courante
-console.log("CurrentURL: " + currentURL);
-const domain = (window.location.host == "localhost") ? "http://localhost/work" : "https://" + window.location.host;     // Obtenir le domaine
-console.log("Domain: " + domain);
-const currentPage = currentURL.substring(currentURL.lastIndexOf("/") + 1);                                              // Obtenir la page courante
-console.log("CurrentPage: " + currentPage);
-const currentLanguage = (window.location.host == "localhost") ? currentURL.slice(22, 24) : currentURL.slice(24, 26);    // Obtenir la langue courante
-console.log("CurrentLanguage: " + currentLanguage);
-const langChange = localStorage.getItem('langChange');                                                                  // Obtenir la valeur stockée de la langue
+/**
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+·······  Définition des variables
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+*/
+const langChangeToggle = document.getElementById('language-checkbox');                                  // Définition du bouton
+const currentURL = document.location.href.replace(/\/$/, "");                                           // Définition de l'URL
+const host = window.location.host;                                                                        // Définition de l'hôte
+const domain = (host == "localhost") ? "http://localhost/work" : `https://${host}`;                       // Définition de la domaine
+const currentPage = currentURL.substring(currentURL.lastIndexOf("/") + 1);                              // Définition de la page
+const currentLanguage = (host == "localhost") ? currentURL.slice(22, 24) : currentURL.slice(24, 26);    // Définition de la langue actuelle
+const toggleLanguage = (currentLanguage == "fr") ? "en" : "fr";                                         // Définition de la langue inverse
+const langChange = localStorage.getItem('langChange');                                                  // Récupération de l'état de la checkbox'
 
-const englishLang = () => {
-    localStorage.setItem('langChange', 'enabled');
-    langChangeToggle.setAttribute('checked', true);
+if (currentLanguage == "en") { document.getElementById('language-checkbox').checked = true; };          // Si la langue est anglais
+if (langChange === 'enabled' && currentLanguage != 'en') { englishLang(); };                            // Si la langue est française
 
-    if (currentLanguage == "fr") {
-
-        const langModeShow = document.querySelectorAll(".langLabel");
-        for (let i = 0; i < langModeShow.length; i++) {
-            langModeShow[i].style.display = "block";
-        }
-
-        if (currentPage != "fr") {
-            window.location.href = domain + "/en/" + currentPage;
-        } else if (currentPage == "fr") {
-            window.location.href = domain + "/en/index.php";
-        }
-    } else if (currentLanguage == "en") { return }
+/**
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+·······  Language switcher
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+*/
+const langSwitcher = () => {                                                                            // Fonction de changement de langue
+    if (currentPage !== currentLanguage) {                                                              // Si la page est différente de la langue
+        window.location.href = `${domain}/${toggleLanguage}/${currentPage}`;                            // Redirection vers la page
+    } else {                                                                                            // Sinon
+        window.location.href = `${domain}/${toggleLanguage}/index.php`;                                 // Redirection vers la page
+    }
 };
 
-const frenchLang = () => {
-    localStorage.setItem('langChange', null);
-    langChangeToggle.setAttribute('checked', false);
-
-    if (currentLanguage == "en") {
-        console.log("Détection langue anglaise");
-        if (currentPage != "en") {
-            console.log("Page courante n'est pas EN");
-            window.location.href = domain + "/fr/" + currentPage;
-
-        } else if (currentPage == "en") {
-            console.log("Page courante est EN");
-            window.location.href = domain + "/fr/index.php";
-        }
-    } else if (currentLanguage == "fr") { return }
-};
-
-if (currentLanguage == "en") { document.getElementById('language-checkbox').checked = true; };
-
-if (langChange === 'enabled' && currentLanguage != 'en') { englishLang(); };
-
-document.getElementById('language-checkbox').addEventListener('click', () => {
-    console.log("Détection du clic");
-    if (localStorage.getItem('langChange') !== 'enabled') {
-        console.log("lancement changement langue vers EN");
-        englishLang();
+/**
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+·······  Ecouteur d'évènements
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+*/
+document.getElementById('language-checkbox').addEventListener('click', () => {                          // Définition de l'écouteur
+    if (langChange !== 'enabled') {
+        langSwitcher();                                                                                 // Définition de la langue
+        localStorage.setItem('langChange', 'enabled');                                                  // Définition de la langue
+        langChangeToggle.setAttribute('checked', true);                                                 // Définition de la langue
     } else {
-        console.log("lancement changement langue vers FR");
-        frenchLang();
+        langSwitcher();                                                                                 // Définition de la langue
+        localStorage.setItem('langChange', null);                                                       // Définition de la langue
+        langChangeToggle.setAttribute('checked', false);                                                // Définition de la langue
     }
 });
 
-// Sources
+/**
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+·······  Sources
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+*/
 // - http://memo-web.fr/categorie-javascript-48/
 // - https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Statements/if...else
 // - https://code.tutsplus.com/tutorials/how-to-change-the-url-in-javascript-redirecting--cms-37323
