a22ab4387f264b02b2da7224277da759159eeebb
aliceout <github.underpay178@slmail.me>
2023-05-13 12:26:21 +0200

    Correction de div

--- en/education.php
+++ en/education.php
@@ -47,54 +47,6 @@ include('../assets/php/header.php');
                                     </ul>
                                 </div>
                             </div>
-                        </div>
-
-                        <div class="divider is-info is-light"></div>
-
-                        <div class="graduation">
-                            <div class="card-content">
-                                <div class="media">
-                                    <div class="media-content">
-                                        <div class="graduationTitle">
-                                            <h5>Master II - Public Policy - International Humanitarian Action</h5>
-                                        </div>
-                                        <div class="lieu">Paris-Est Créteil University</div>
-                                        <span class="date">2018 - 2019</span>
-                                    </div>
-                                    <div class="media-left">
-                                        <div class="logo"><img src="../assets/images/upec.png"></div>
-                                    </div>
-                                </div>
-                                <div class="content">
-                                    <p><strong>Option</strong>: Emergency and Rehabilitation<br> Graduated with AB mention</p><br />
-                                    <p>Project cycle management, quality monitoring, evaluation and capitalization <br>
-                                        Country cases, scenarios, project designs, strengthening and partnerships<br><br> <strong>Internship thesis I</strong>: <i>"ICTs in the international solidarity sector, reflections on a forced integration"</i><br> <strong>Internship thesis II</strong>: <i>"The notion of crisis and its use in the migration theme, from a mobilization tool to the abuse of an object that has become political"</i>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="divider is-info is-light"></div>
-
-                        <div class="graduation">
-                            <div class="card-content">
-                                <div class="media">
-                                    <div class="media-content">
-                                        <div class="graduationTitle">
-                                            <h5>Master I - Political Science</h5>
-                                        </div>
-                                        <div class="lieu">Lyon III Jean Moulin University</div>
-                                        <span class="date">2017 - 2018</span>
-                                    </div>
-                                    <div class="media-left">
-                                        <div class="logo"><img src="../assets/images/lyon3.png"></div>
-                                    </div>
-                                </div>
-                                <div class="content">
-                                    <p><strong>Mention</strong> : International Relations <br>
-                                        Graduate<br><br> International Crisis Management Simulation, Global Cultural Areas<br> Religions and Globalization, European Political Economy, Comparative Foreign Policies<br><br> <strong>Dissertation</strong>: <i>"The Outsourcing of Migration Controls in the Sahel. Study on a new tool of an extra-territorialized European border management policy"</i></p>
-                                </div>
-                            </div>
 
                             <div class="divider is-info is-light"></div>
 
@@ -103,17 +55,20 @@ include('../assets/php/header.php');
                                     <div class="media">
                                         <div class="media-content">
                                             <div class="graduationTitle">
-                                                <h5>Licence · Law</h5>
+                                                <h5>Master II - Public Policy - International Humanitarian Action</h5>
                                             </div>
-                                            <div class="lieu">Toulouse I Capitole University</div>
-                                            <span class="date">2015 - 2017</span>
+                                            <div class="lieu">Paris-Est Créteil University</div>
+                                            <span class="date">2018 - 2019</span>
                                         </div>
                                         <div class="media-left">
-                                            <div class="logo"><img src="../assets/images/toulouse1.png"></div>
+                                            <div class="logo"><img src="../assets/images/upec.png"></div>
                                         </div>
                                     </div>
                                     <div class="content">
-                                        <p><strong>Mention</strong>: Public Law<br>Diploma mention AB<br><br> Dominant in administrative law, international law and European law</p>
+                                        <p><strong>Option</strong>: Emergency and Rehabilitation<br> Graduated with AB mention</p><br />
+                                        <p>Project cycle management, quality monitoring, evaluation and capitalization <br>
+                                            Country cases, scenarios, project designs, strengthening and partnerships<br><br> <strong>Internship thesis I</strong>: <i>"ICTs in the international solidarity sector, reflections on a forced integration"</i><br> <strong>Internship thesis II</strong>: <i>"The notion of crisis and its use in the migration theme, from a mobilization tool to the abuse of an object that has become political"</i>
+                                        </p>
                                     </div>
                                 </div>
                             </div>
@@ -125,26 +80,70 @@ include('../assets/php/header.php');
                                     <div class="media">
                                         <div class="media-content">
                                             <div class="graduationTitle">
-                                                <h5>Capacity in Law</h5>
+                                                <h5>Master I - Political Science</h5>
                                             </div>
-                                            <div class="lieu">National Conservatory of Arts and Crafts</div>
-                                            <span class="date">2013 - 2015</span>
+                                            <div class="lieu">Lyon III Jean Moulin University</div>
+                                            <span class="date">2017 - 2018</span>
                                         </div>
                                         <div class="media-left">
-                                            <div class="logo"><img src="../assets/images/cnam.png"></div>
+                                            <div class="logo"><img src="../assets/images/lyon3.png"></div>
                                         </div>
                                     </div>
                                     <div class="content">
-                                        <p>Degree issued by the University Toulouse I Capitole <br>Diploma mention B</p>
+                                        <p><strong>Mention</strong> : International Relations <br>
+                                            Graduate<br><br> International Crisis Management Simulation, Global Cultural Areas<br> Religions and Globalization, European Political Economy, Comparative Foreign Policies<br><br> <strong>Dissertation</strong>: <i>"The Outsourcing of Migration Controls in the Sahel. Study on a new tool of an extra-territorialized European border management policy"</i></p>
+                                    </div>
+                                </div>
+
+                                <div class="divider is-info is-light"></div>
+
+                                <div class="graduation">
+                                    <div class="card-content">
+                                        <div class="media">
+                                            <div class="media-content">
+                                                <div class="graduationTitle">
+                                                    <h5>Licence · Law</h5>
+                                                </div>
+                                                <div class="lieu">Toulouse I Capitole University</div>
+                                                <span class="date">2015 - 2017</span>
+                                            </div>
+                                            <div class="media-left">
+                                                <div class="logo"><img src="../assets/images/toulouse1.png"></div>
+                                            </div>
+                                        </div>
+                                        <div class="content">
+                                            <p><strong>Mention</strong>: Public Law<br>Diploma mention AB<br><br> Dominant in administrative law, international law and European law</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="divider is-info is-light"></div>
+
+                                <div class="graduation">
+                                    <div class="card-content">
+                                        <div class="media">
+                                            <div class="media-content">
+                                                <div class="graduationTitle">
+                                                    <h5>Capacity in Law</h5>
+                                                </div>
+                                                <div class="lieu">National Conservatory of Arts and Crafts</div>
+                                                <span class="date">2013 - 2015</span>
+                                            </div>
+                                            <div class="media-left">
+                                                <div class="logo"><img src="../assets/images/cnam.png"></div>
+                                            </div>
+                                        </div>
+                                        <div class="content">
+                                            <p>Degree issued by the University Toulouse I Capitole <br>Diploma mention B</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+                </main>
             </div>
-            </main>
         </div>
-    </div>
     </div>
 </body>
 <?php include('../assets/php/footer.php'); ?>
