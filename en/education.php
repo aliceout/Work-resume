c6b6ba2d2f84832e879da69bf80e9e6f2d0e8fd5
aliceout <github.underpay178@slmail.me>
2023-05-13 12:22:48 +0200

    Changement logo oclock

--- en/education.php
+++ en/education.php
@@ -1,132 +1,152 @@
 <!doctype html>
 <html xml:lang="fr" lang="fr">
 
-    <?php
-    $currentPage = 'education';
-    include('../assets/php/header.php');
-    ?>
+<?php
+$currentPage = 'education';
+include('../assets/php/header.php');
+?>
 
-    <body>
-        <div class="container">
-            <div id="section" class="section">
+<body>
+    <div class="container">
+        <div id="section" class="section">
 
-                <div id="big-main" class="columns is-multiline">
-                    <?php include('sidebar.php'); ?>
-                    <main class="column">
+            <div id="big-main" class="columns is-multiline">
+                <?php include('sidebar.php'); ?>
+                <main class="column">
 
-                        <div class="card">
+                    <div class="card">
+                        <div class="card-content">
+                            <h2>University graduation</h2>
+
+                            <div class="graduation">
+                                <div class="card-content">
+                                    <p>For certifications and short courses, see <a href="skills.php">Languages &amp; Skills</a> page.</p>
+                                </div>
+                            </div>
+
+                            <div class="divider is-info is-light"></div>
+
+                            <div class="graduation">
+                                <div class="card-content">
+                                    <div class="media">
+                                        <div class="media-content">
+                                            <div class="graduationTitle">
+                                                <h5>Professionnal title · Web and Mobile Web Developer - FullStack JavaScript</h5>
+                                            </div>
+                                            <div class="lieu">O'Clock scholl</div>
+                                            <span class="date">2023 · In progress</span>
+                                        </div>
+                                        <div class="media-left">
+                                            <div class="logo"><img src="../assets/images/oclock-long.png"></div>
+                                        </div>
+                                    </div>
+                                    <ul>
+                                        <li>- Frontend : HTML, CSS, JavaScript, React</li>
+                                        <li>- Backend : Node.js, PostgreSQL, SequeLize</li>
+                                        <li>- Specialization : React - Redux</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="divider is-info is-light"></div>
+
+                        <div class="graduation">
                             <div class="card-content">
-                                <h2>University graduation</h2>
-
-                                <div class="graduation">
-                                    <div class="card-content">
-                                        <p>For certifications and short courses, see <a href="skills.php">Languages &amp; Skills</a> page.</p>
+                                <div class="media">
+                                    <div class="media-content">
+                                        <div class="graduationTitle">
+                                            <h5>Master II - Public Policy - International Humanitarian Action</h5>
+                                        </div>
+                                        <div class="lieu">Paris-Est Créteil University</div>
+                                        <span class="date">2018 - 2019</span>
+                                    </div>
+                                    <div class="media-left">
+                                        <div class="logo"><img src="../assets/images/upec.png"></div>
                                     </div>
                                 </div>
+                                <div class="content">
+                                    <p><strong>Option</strong>: Emergency and Rehabilitation<br> Graduated with AB mention</p><br />
+                                    <p>Project cycle management, quality monitoring, evaluation and capitalization <br>
+                                        Country cases, scenarios, project designs, strengthening and partnerships<br><br> <strong>Internship thesis I</strong>: <i>"ICTs in the international solidarity sector, reflections on a forced integration"</i><br> <strong>Internship thesis II</strong>: <i>"The notion of crisis and its use in the migration theme, from a mobilization tool to the abuse of an object that has become political"</i>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
 
-<div class="divider is-info is-light"></div>
+                        <div class="divider is-info is-light"></div>
 
-<div class="graduation">
-    <div class="card-content">
-        <div class="media">
-            <div class="media-content">
-                <div class="graduationTitle"><h5>Professionnal title · Web and Mobile Web Developer - FullStack JavaScript</h5></div>
-                <div class="lieu">O'Clock scholl</div>
-                <span class="date">2023 · In progress</span></div>
-            <div class="media-left">
-                <div class="logo"><img src="../assets/images/oclock.png"></div>
-            </div>
-        </div>
-                <ul>
-                    <li>- Frontend : HTML, CSS, JavaScript, React</li>
-                    <li>- Backend : Node.js, PostgreSQL, SequeLize</li>
-                    <li>- Specialization : React - Redux</li>
-            </ul></div>
-    </div>
-</div>
-
-                                <div class="divider is-info is-light"></div>
-
-                                <div class="graduation">
-                                    <div class="card-content">
-                                        <div class="media">
-                                            <div class="media-content">
-                                                <div class="graduationTitle"><h5>Master II - Public Policy - International Humanitarian Action</h5></div>
-                                                <div class="lieu">Paris-Est Créteil University</div>
-                                                <span class="date">2018 - 2019</span></div>
-                                            <div class="media-left">
-                                                <div class="logo"><img src="../assets/images/upec.png"></div>
-                                            </div>
+                        <div class="graduation">
+                            <div class="card-content">
+                                <div class="media">
+                                    <div class="media-content">
+                                        <div class="graduationTitle">
+                                            <h5>Master I - Political Science</h5>
                                         </div>
-                                        <div class="content"><p><strong>Option</strong>: Emergency and Rehabilitation<br> Graduated with AB mention</p><br /> <p>Project cycle management, quality monitoring, evaluation and capitalization <br>
-                                            Country cases, scenarios, project designs, strengthening and partnerships<br><br> <strong>Internship thesis I</strong>: <i>"ICTs in the international solidarity sector, reflections on a forced integration"</i><br> <strong>Internship thesis II</strong>: <i>"The notion of crisis and its use in the migration theme, from a mobilization tool to the abuse of an object that has become political"</i>
-                                            </p></div>
+                                        <div class="lieu">Lyon III Jean Moulin University</div>
+                                        <span class="date">2017 - 2018</span>
+                                    </div>
+                                    <div class="media-left">
+                                        <div class="logo"><img src="../assets/images/lyon3.png"></div>
                                     </div>
                                 </div>
+                                <div class="content">
+                                    <p><strong>Mention</strong> : International Relations <br>
+                                        Graduate<br><br> International Crisis Management Simulation, Global Cultural Areas<br> Religions and Globalization, European Political Economy, Comparative Foreign Policies<br><br> <strong>Dissertation</strong>: <i>"The Outsourcing of Migration Controls in the Sahel. Study on a new tool of an extra-territorialized European border management policy"</i></p>
+                                </div>
+                            </div>
 
-                                <div class="divider is-info is-light"></div>
+                            <div class="divider is-info is-light"></div>
 
-                                <div class="graduation">
-                                    <div class="card-content">
-                                        <div class="media">
-                                            <div class="media-content">
-                                                <div class="graduationTitle"><h5>Master I - Political Science</h5></div>
-                                                <div class="lieu">Lyon III Jean Moulin University</div>
-                                                <span class="date">2017 - 2018</span>
+                            <div class="graduation">
+                                <div class="card-content">
+                                    <div class="media">
+                                        <div class="media-content">
+                                            <div class="graduationTitle">
+                                                <h5>Licence · Law</h5>
                                             </div>
-                                            <div class="media-left">
-                                                <div class="logo"><img src="../assets/images/lyon3.png"></div>
-                                            </div>
+                                            <div class="lieu">Toulouse I Capitole University</div>
+                                            <span class="date">2015 - 2017</span>
                                         </div>
-                                        <div class="content"><p><strong>Mention</strong> : International Relations <br>
-                                            Graduate<br><br> International Crisis Management Simulation, Global Cultural Areas<br> Religions and Globalization, European Political Economy, Comparative Foreign Policies<br><br> <strong>Dissertation</strong>: <i>"The Outsourcing of Migration Controls in the Sahel. Study on a new tool of an extra-territorialized European border management policy"</i></p>
-                                        </div>
-                                    </div>
-
-                                    <div class="divider is-info is-light"></div>
-
-                                    <div class="graduation">
-                                        <div class="card-content">
-                                            <div class="media">
-                                                <div class="media-content">
-                                                    <div class="graduationTitle"><h5>Licence · Law</h5></div>
-                                                    <div class="lieu">Toulouse I Capitole University</div>
-                                                    <span class="date">2015 - 2017</span>
-                                                </div>
-                                                <div class="media-left">
-                                                    <div class="logo"><img src="../assets/images/toulouse1.png"></div>
-                                                </div>
-                                            </div>
-                                            <div class="content"><p><strong>Mention</strong>: Public Law<br>Diploma mention AB<br><br> Dominant in administrative law, international law and European law</p>
-                                            </div>
+                                        <div class="media-left">
+                                            <div class="logo"><img src="../assets/images/toulouse1.png"></div>
                                         </div>
                                     </div>
+                                    <div class="content">
+                                        <p><strong>Mention</strong>: Public Law<br>Diploma mention AB<br><br> Dominant in administrative law, international law and European law</p>
+                                    </div>
+                                </div>
+                            </div>
 
-                                    <div class="divider is-info is-light"></div>
+                            <div class="divider is-info is-light"></div>
 
-                                    <div class="graduation">
-                                        <div class="card-content">
-                                            <div class="media">
-                                                <div class="media-content">
-                                                    <div class="graduationTitle"><h5>Capacity in Law</h5></div>
-                                                    <div class="lieu">National Conservatory of Arts and Crafts</div>
-                                                    <span class="date">2013 - 2015</span>
-                                                </div>
-                                                <div class="media-left">
-                                                    <div class="logo"><img src="../assets/images/cnam.png"></div>
-                                                </div>
+                            <div class="graduation">
+                                <div class="card-content">
+                                    <div class="media">
+                                        <div class="media-content">
+                                            <div class="graduationTitle">
+                                                <h5>Capacity in Law</h5>
                                             </div>
-                                            <div class="content"><p>Degree issued by the University Toulouse I Capitole <br>Diploma mention B</p>
-                                            </div>
+                                            <div class="lieu">National Conservatory of Arts and Crafts</div>
+                                            <span class="date">2013 - 2015</span>
                                         </div>
+                                        <div class="media-left">
+                                            <div class="logo"><img src="../assets/images/cnam.png"></div>
+                                        </div>
+                                    </div>
+                                    <div class="content">
+                                        <p>Degree issued by the University Toulouse I Capitole <br>Diploma mention B</p>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </main>
-                </div>
+                    </div>
             </div>
+            </main>
         </div>
-    </body>
-    <?php include('../assets/php/footer.php'); ?>
+    </div>
+    </div>
+</body>
+<?php include('../assets/php/footer.php'); ?>
+
 </html>
--- en/index.php
+++ en/index.php
@@ -1,99 +1,101 @@
 <!doctype html>
 <html xml:lang="fr" lang="fr">
 
-    <?php
-    $currentPage = 'index';
-    include('../assets/php/header.php');
-    ?>
+<?php
+$currentPage = 'index';
+include('../assets/php/header.php');
+?>
 
-    <body>
-        <div class="container">
-            <div id="section" class="section">
+<body>
+    <div class="container">
+        <div id="section" class="section">
 
-                <div id="big-main" class="columns is-multiline">
-                    <?php include('sidebar.php'); ?>
-                    <main class="column">
-                        <div class="columns sub-main"> 
-                            <section class="column">
+            <div id="big-main" class="columns is-multiline">
+                <?php include('sidebar.php'); ?>
+                <main class="column">
+                    <div class="columns sub-main">
+                        <section class="column">
 
-                                <div class="card card-blue">
-                                    <div class="card-content">
-                                        <div class="about">
-                                            <h3>Bonjour,</h3>
-                                            <p>
+                            <div class="card card-blue">
+                                <div class="card-content">
+                                    <div class="about">
+                                        <h3>Bonjour,</h3>
+                                        <p>
                                             After ten years of working in the fields of international solidarity and freight transport, I decided to give a new professional impetus to my life, by moving towards a job that I am passionate about, corresponding to my professional and personal expectations and aspirations.<br><br>
                                             Thanks to these experiences and my previous university education (in law and international relations) which allowed me to acquire many skills and knowledge, I decided to start a professional reconversion towards the web and mobile web development.<br><br>
                                             It is for this reason that I started a training of "Full Stack Javascript Developer" in February 2023 in the O'Clock school.<br><br>
                                             Curious, creative, autonomous, proactive, dynamic, organized and used to teamwork, I know how to adapt to all situations and work environment.<br><br>
-                                            I have always been passionate about the world of technology, and I have already developed many personal projects. I am enthusiastic about the upcoming challenges and discoveries. 
-                                                <br>
-                                            </p>
-                                        </div>
+                                            I have always been passionate about the world of technology, and I have already developed many personal projects. I am enthusiastic about the upcoming challenges and discoveries.
+                                            <br>
+                                        </p>
                                     </div>
                                 </div>
-                            </section>
+                            </div>
+                        </section>
 
-                            <section id="index-recent" class="column">
-                                <div class="card card-index">
-                                    <div class="card-content">
-                                        <div class="media">
-                                            <div class="media-content">
-                                                <h3>GITHUB PROFIL</h3>
-                                                <h5><a href="https://github.com/aliceout" target="_blank">aliceout (Alice OsDel)</a></h5>
-                                            </div>
-                                            <div class="media-left">
-                                                <figure>
-                                                    <div class="logo"><img src="../assets/images/github.png" alt="Github"></div>
-                                                </figure>
-                                            </div>
+                        <section id="index-recent" class="column">
+                            <div class="card card-index">
+                                <div class="card-content">
+                                    <div class="media">
+                                        <div class="media-content">
+                                            <h3>GITHUB PROFIL</h3>
+                                            <h5><a href="https://github.com/aliceout" target="_blank">aliceout (Alice OsDel)</a></h5>
+                                        </div>
+                                        <div class="media-left">
+                                            <figure>
+                                                <div class="logo"><img src="../assets/images/github.png" alt="Github"></div>
+                                            </figure>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="card card-index">
-                                    <div class="card-content">
-                                        <div class="media">
-                                            <div class="media-content">
-                                                <h3>Current training</h3>
-                                                <h5>Full Stack Javascript Developer</h5>
-                                                <h6>O'CLOCK School<br>2023</h6>
-                                            </div>
-                                            <div class="media-left">
-                                                <figure>
-                                                    <div class="logo"><img src="../assets/images/oclock.png" alt="O'Clock"></div>
-                                                </figure>
-                                            </div>
+                            </div>
+                            <div class="card card-index">
+                                <div class="card-content">
+                                    <div class="media">
+                                        <div class="media-content">
+                                            <h3>Current training</h3>
+                                            <h5>Full Stack Javascript Developer</h5>
+                                            <h6>O'CLOCK School<br>2023</h6>
                                         </div>
-                                        <div class="content">
-                                            <a href="education.php">En savoir plus</a>
+                                        <div class="media-left">
+                                            <figure>
+                                                <div class="logo"><img src="../assets/images/oclock-round.png" alt="O'Clock"></div>
+                                            </figure>
                                         </div>
                                     </div>
+                                    <div class="content">
+                                        <a href="education.php">En savoir plus</a>
+                                    </div>
                                 </div>
+                            </div>
 
-                                <div class="card card-index">
-                                    <div class="card-content">
-                                        <div class="media">
-                                            <div class="media-content">
-                                                <h3>Last experience</h3>
-                                                <h5>MÉDECINS DU MONDE FRANCE</h5>
-                                                <h6>Logistics Coordinator</h6>
-                                                <div class="date">2021-2022</div>
-                                            </div>
-                                            <div class="media-left">
-                                                <figure>
-                                                    <div class="logo"><img src="../assets/images/mdm.png" alt="Bioforce"></div>
-                                                </figure>
-                                            </div>
+                            <div class="card card-index">
+                                <div class="card-content">
+                                    <div class="media">
+                                        <div class="media-content">
+                                            <h3>Last experience</h3>
+                                            <h5>MÉDECINS DU MONDE FRANCE</h5>
+                                            <h6>Logistics Coordinator</h6>
+                                            <div class="date">2021-2022</div>
                                         </div>
-                                        <div class="content">
-                                            <p>Emergency responses to the health needs of populations affected by the conflict in the Central African Republic<br>
-                                                <a href="experiences.php">Read more</a></p>
+                                        <div class="media-left">
+                                            <figure>
+                                                <div class="logo"><img src="../assets/images/mdm.png" alt="Bioforce"></div>
+                                            </figure>
                                         </div>
                                     </div>
+                                    <div class="content">
+                                        <p>Emergency responses to the health needs of populations affected by the conflict in the Central African Republic<br>
+                                            <a href="experiences.php">Read more</a>
+                                        </p>
+                                    </div>
                                 </div>
-                                </main>
-                        </div>
-                        </div>
-                </div>
-                </body>
-            <?php include('../assets/php/footer.php'); ?>
-            </html>
+                            </div>
+                </main>
+            </div>
+        </div>
+    </div>
+</body>
+<?php include('../assets/php/footer.php'); ?>
+
+</html>
