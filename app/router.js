b1155e3ae477a6851d8e67bf975d8dd592a2be2b
aliceout <github.underpay178@slmail.me>
2023-05-31 01:29:20 +0200

    Récriture avec EJS fonctionelle (reste à reconstruire la trad en anglais)

--- app/router.js
+++ app/router.js
@@ -6,27 +6,34 @@
 import express from 'express';                                       // Import du module ExpressJS 
 const router = express.Router();                                     // Import du module routeur d'ExpressJS
 
-// Import des controllers
-import { mainController } from './controllers/mainController.js';    // Import du controller des listes
-
 /*
 ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 ·······  2- Definition des routes
 ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
-*/
+*/     
+/// Partie française
+router.get('/', (req, res) => {res.render("fr/index")});
+router.get('/index', (req, res) => {res.render("fr/index")});
+router.get('/education', (req, res) => {res.render("fr/education")});
+router.get('/experiences', (req, res) => {res.render("fr/experiences")});
+router.get('/benevolat', (req, res) => {res.render("fr/benevolat")});
+router.get('/personnal', (req, res) => {res.render("fr/personnal")});
+router.get('/skills', (req, res) => {res.render("fr/skills")});
+router.get('/contact', (req, res) => {res.redirect("https://join.skype.com/invite/II99wpbBBR78")});
+router.get('/resume', (req, res) => {res.redirect("https://cloud.ofanch.me/s/nCnZ5W7AM5adape#pdfviewer")});
 
-router.get('/currencies', mainController.getCurrenciesList);         // récupérer la liste des devises
-
-router.get('/latest/base_currency/:fromCurrencie/currencies/:toCurrencie', mainController.getLatestRate); 
-
-router.get('/historical/currencies/:currencies/base_currency/:base_currency/date_from/:dateFrom/date_to/:dateTo', mainController.history_rates); 
-
-// /historical/currencies/USD,EUR,CHF,GBP,CNY,INR,RUB/base_currency/HRK/date_from/2023-05-26/date_to/2023-05-26
-
+/// Partie anglaise
+router.get('/', (req, res) => {res.render("en/index")});
+router.get('/index', (req, res) => {res.render("en/index")});
+router.get('/education', (req, res) => {res.render("en/education")});
+router.get('/experiences', (req, res) => {res.render("en/experiences")});
+router.get('/benevolat', (req, res) => {res.render("en/benevolat")});
+router.get('/personnal', (req, res) => {res.render("en/personnal")});
+router.get('/skills', (req, res) => {res.render("en/skills")});
 
 /**
 ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 ·······  Export
 ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 */
-export { router };
+export {router};
