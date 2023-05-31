8000f636740f251618a8860a208c46a1ad28e3d0
aliceout <github.underpay178@slmail.me>
2023-05-31 20:58:14 +0200

    Correction de conflit entre PurgeCSS et darkmode

--- tools/.purgecss.config.cjs
+++ tools/.purgecss.config.cjs
@@ -1,4 +1,7 @@
 module.exports = {
     content: ['app/views/fr/*', 'app/views/en*', 'app/views/partials/*', 'app/views/layouts/*'],
-    css: ['public/css/style.css']
+    css: ['public/css/style.css'],
+    safelist: {
+        standard: ['dark-mode'],
+    }
 }
