0ea6489891483914c535f677f5ec0c8449ec3cf6
aliceout <github.underpay178@slmail.me>
2023-05-31 17:14:26 +0200

    Travail en cours sur dynamisation des langues

---
+++ public/js/langchanger.js
@@ -0,0 +1,28 @@
+/**
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+·······  Ecouteur d'évènements
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+*/
+const langChangeToggle = document.getElementById('language-checkbox');
+langChange = (langChangeToggle.setAttribute('checked', true)) ? "en" : "fr";
+
+
+document.getElementById('language-checkbox').addEventListener('click', () => {                
+    if (langChange == 'en') {        
+        req.session.lang = "en"    
+        console.log(req.sessionlang);                                                                         
+
+    } else {
+        req.session.lang = "fr"    
+        console.log(req.session.lang);          
+    }
+});
+
+/**
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+·······  Définition des variables
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+*/
+const currentLanguage = localStorage.getItem('lang');                                                
+
+
