a99a4ee4a65526f1cfc021c9d7074c71eeebda49
aliceout <github.underpay178@slmail.me>
2023-11-17 00:25:31 +0100

    Navbar en cours

--- data/navigation.js
+++ data/navigation.js
@@ -1,1 +1,27 @@
-module.export = {};
+module.exports = [
+  {
+    name: "Home",
+    icon: "FaHome",
+    href: "/",
+  },
+  {
+    name: "Experiences",
+    icon: "FaHome",
+    href: "/works",
+  },
+  {
+    name: "Etudes",
+    icon: "FaHome",
+    href: "/",
+  },
+  {
+    name: "Portfolio",
+    icon: "FaHome",
+    href: "/",
+  },
+  {
+    name: "Personnel",
+    icon: "FaHome",
+    href: "/",
+  },
+];
