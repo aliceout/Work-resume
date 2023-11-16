db9703cf675131974b2a4d395a830bc779d2828d
Alice OsDel <github.underpay178@slmail.me>
2023-11-16 18:04:48 +0100

    Structure posée

---
+++ components/layout/Navbar.jsx
@@ -0,0 +1,22 @@
+export default function Navbar() {
+  return (
+    <nav className="flex items-center justify-end h-20 px-4 my-auto bg-purple-200 rounded-lg gap-x-4">
+      <div className="items-center justify-center bg-gray-200 w-14 h-14 aspect-square">
+        <img></img>
+        <p></p>
+      </div>
+      <div className="items-center justify-center bg-gray-200 w-14 h-14 aspect-square">
+        <img></img>
+        <p></p>
+      </div>
+      <div className="items-center justify-center bg-gray-200 w-14 h-14 aspect-square">
+        <img></img>
+        <p></p>
+      </div>
+      <div className="items-center justify-center bg-gray-200 w-14 h-14 aspect-square">
+        <img></img>
+        <p></p>
+      </div>
+    </nav>
+  );
+}
---
+++ pages/_app.jsx
@@ -0,0 +1,32 @@
+import { ThemeProvider } from "next-themes";
+import Container from "../components/layout/Container";
+import Header from "../components/layout/Header";
+import Sidebar from "../components/layout/Sidebar";
+import Navbar from "../components/layout/Navbar";
+import "../styles/globals.css";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider attribute="class">
+        <Container>
+            <Header />
+          <div
+            id="app"
+            className="flex  content-start h-full w-full gap-10 mt-[200px] "
+          >
+              <Sidebar />
+            <div
+              id="panel"
+              className="flex flex-col xl:basis-9/12 gap-y-10"
+            >
+                <Navbar />
+              <main className="flex flex-col flex-1 p-5 bg-green-200 rounded-lg">
+                <Component {...pageProps} />
+              </main>
+            </div>
+          </div>
+        </Container>
+
+    </ThemeProvider>
+  );
+}
