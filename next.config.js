2f0a3e25fd289d3c73ceb857a46c7471e4b0ac5c
Alice OsDel <github.underpay178@slmail.me>
2023-11-23 18:16:39 +0100

    Corrections sur détection de la langue par defaut

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {};
 module.exports = nextConfig;
 module.exports = {
   i18n: {
-    defaultLocale: 'fr',
-    locales: ['fr', 'en'],
+    defaultLocale: "fr",
+    locales: ["fr", "en"],
+    localeDetection: false,
   },
 };
