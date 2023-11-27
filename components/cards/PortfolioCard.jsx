d0bee94b9a8653f85a5e4e1b1c7ddde2b162b6c6
Alice OsDel <github.underpay178@slmail.me>
2023-11-27 12:43:41 +0100

    Micro changements

--- components/cards/PortfolioCard.jsx
+++ components/cards/PortfolioCard.jsx
@@ -13,7 +13,7 @@ export default function PortfolioCard({ project }) {
   return (
     <article className="flex flex-row items-center flex-1 w-full gap-x-10">
       <div className="flex flex-col justify-between transition duration-300 shadow xl:w-10/12 xl:overflow-visible xl:flex-row rounded-xl bg-gray-50/40 dark:bg-slate-900/80">
-        <div className="flex flex-col justify-between w-full px-2 pt-8 pb-4 xl:pr-0 xl:pl-10 xl:py-8 xl:basis-7/12 basis-full">
+        <div className="flex flex-col justify-between w-full px-2 pt-8 pb-4 xl:pr-0 xl:pl-10 xl:py-8 xl:basis-8/12 basis-full">
           <div className="flex flex-col gap-y-1">
             <div className="flex flex-row items-center justify-start gap-x-2">
               <p className="text-xl font-bold text-primary dark:text-secondary">
@@ -65,7 +65,7 @@ export default function PortfolioCard({ project }) {
             </Link>
           </div>
         </div>
-        <div className="flex mx-auto md:mb-4 xl:my-10 xl:ml-5 xl:-mr-36 drop-shadow-lg rounded-xl">
+        <div className="flex mx-auto md:mb-4 xl:my-10 xl:ml-14 xl:-mr-28 drop-shadow-lg rounded-xl">
           <Image
             src={project.picture}
             width={400}
