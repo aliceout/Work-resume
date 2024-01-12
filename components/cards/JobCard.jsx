495ce5fa973026af292130bca87b1ac859fb54c3
Alice OsDel <github.underpay178@slmail.me>
2024-01-12 13:27:43 +0100

    Correction de l'affichage des entreprises/ONG en mode sombre

--- components/cards/JobCard.jsx
+++ components/cards/JobCard.jsx
@@ -10,7 +10,7 @@ export default function JobCard({ job }) {
     <article className="flex w-full p-4">
       <div className="flex flex-col w-full gap-y-5">
         <div className="flex flex-row justify-between w-full gap-x-10">
-          <div className="flex flex-col w-full dark:md:basis-full basis-7/12 md:basis-9/12 dark:lg:basis-10/12">
+          <div className="flex flex-col w-fullbasis-7/12 md:basis-9/12 ">
             <h3 className="text-xl font-bold text-slate-800 dark:text-slate-100">
               {job.company}
             </h3>
@@ -25,22 +25,20 @@ export default function JobCard({ job }) {
               </div>
             </div>
           </div>
-          {theme !== "dark" && (
-            <div className="flex items-center justify-center w-full px-3 overflow-hidden rounded-lg basis-5/12 md:basis-3/12">
-              <Image
-                src={job.logo}
-                alt={job.company}
-                width={450}
-                height={300}
-                style={{
-                  width: "100% !important",
-                  position: "relative !important",
-                  height: "unset !important",
-                }}
-                className="w-full h-auto rounded-lg"
-              />
-            </div>
-          )}
+          <div className="flex items-center justify-center w-full px-3 overflow-hidden rounded-lg basis-5/12 md:basis-3/12">
+            <Image
+              src={job.logo}
+              alt={job.company}
+              width={450}
+              height={300}
+              style={{
+                width: "100% !important",
+                position: "relative !important",
+                height: "unset !important",
+              }}
+              className="w-full h-auto rounded-lg dark:brightness-75 "
+            />
+          </div>
         </div>
         <div className="flex flex-col text-sm dark:text-gray-200 gap-y-2 lg:w-10/12">
           <ReactMarkdown>{job.description}</ReactMarkdown>
