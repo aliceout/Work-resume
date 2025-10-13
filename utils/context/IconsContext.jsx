75869ef75aed3e00c4c20c84dd1d4d1e8043c9c7
aliceout <github.underpay178@slmail.me>
2025-10-13 12:56:39 +0300

    Allégement des imports de pack d'icone

--- utils/context/IconsContext.jsx
+++ utils/context/IconsContext.jsx
@@ -1,39 +1,46 @@
 import { createContext, useContext } from "react";
 
-import * as IoIcons from "react-icons/io";
-import * as GrIcons from "react-icons/gr";
-import * as BsIcons from "react-icons/bs";
-import * as FaIcons from "react-icons/fa";
-import * as Io5Icons from "react-icons/io5";
-import * as MdIcons from "react-icons/md";
-import * as SlIcons from "react-icons/sl";
-import * as SiIcons from "react-icons/si";
-import * as CiIcones from "react-icons/ci";
-import * as PiIcons from "react-icons/pi";
-import * as RiIcons from "react-icons/ri";
+// N’IMPORTER QUE CE QUI EST UTILISÉ DANS TES DATA/COMPOSANTS
+import {
+  FaGithub,
+  FaLinkedinIn,
+  FaUniversity,
+  FaHandsHelping,
+  FaHome,
+} from "react-icons/fa";
+import { MdOutlinePhotoLibrary, MdPlace } from "react-icons/md";
+import { RiRobot2Fill } from "react-icons/ri";
+import { GrUserWorker } from "react-icons/gr";
+import { IoCloseSharp } from "react-icons/io5";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { SiProtonmail, SiSignal } from "react-icons/si";
+import { BsMicrosoftTeams } from "react-icons/bs";
 
-const IconsContext = createContext();
+const IconsContext = createContext({
+  FaGithub,
+  FaLinkedinIn,
+  FaUniversity,
+  FaHandsHelping,
+  FaHome,
+  MdOutlinePhotoLibrary,
+  MdPlace,
+  RiRobot2Fill,
+  GrUserWorker,
+  IoCloseSharp,
+  GiHamburgerMenu,
+  SiProtonmail,
+  SiSignal,
+  BsMicrosoftTeams,
+});
 
 export function useIcons() {
   return useContext(IconsContext);
 }
 
 export function IconsProvider({ children }) {
-  const allIcons = {
-    ...IoIcons,
-    ...GrIcons,
-    ...BsIcons,
-    ...FaIcons,
-    ...Io5Icons,
-    ...MdIcons,
-    ...SlIcons,
-    ...SiIcons,
-    ...CiIcones,
-    ...PiIcons,
-    ...RiIcons,
-  };
-
   return (
-    <IconsContext.Provider value={allIcons}>{children}</IconsContext.Provider>
+    <IconsContext.Provider value={IconsContext._currentValue}>
+      {children}
+    </IconsContext.Provider>
   );
 }
