ed52cd4b9cc2b3a17778dd90b3f60b291f71df30
aliceout <github.underpay178@slmail.me>
2025-10-19 12:44:42 +0300

    Corrections pour build serveur lighter

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n: {
     defaultLocale: "fr",
     locales: ["fr", "en"],
     localeDetection: false,
   },
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
